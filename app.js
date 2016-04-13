fa7a54e3b947b2d1ae2fc8df348f636e0c78a127
pierreolivier <bouteau.pierreolivier@gmail.com>
2016-04-13 15:39:43 +0200

    remove message

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var bot = require('./lib/discord-bot');
 exports.run = function() {
     bot.run();
 };
+
+process.on('uncaughtException', function ( err ) {
+    console.error(err.stack);
+
+    bot.stop();
+
+    bot.run();
+});
