d18003724f68fd5ceee1e3b02d09d549df7b8bb4
pierreolivier <bouteau.pierreolivier@gmail.com>
2016-04-14 14:13:08 +0200

    voice (tts)

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 var bot = require('./lib/discord-bot');
+var log = require('util').log;
 
 exports.run = function() {
+    console.log('');
+    log('Bot starting...');
+
     bot.run();
 };
 
 process.on('uncaughtException', function ( err ) {
+    log('Error:');
     console.error(err.stack);
+    log('Bot restarting...');
 
     bot.stop();
 
     bot.run();
+});
+
+function stop() {
+    log('Bot stoping...');
+
+    bot.stop();
+    setTimeout(function() {
+        process.exit(0);
+    }, 1000);
+}
+
+process.on('SIGINT', function() {
+    stop();
+});
+
+process.on('SIGTERM', function() {
+    stop();
 });
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+node ./bin/www 2>&1 >> /tmp/discord-bot-log
